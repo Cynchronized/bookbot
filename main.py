d146b9889c4843bfff5f1c1f19899a91218b151d
Cynchronized <austin.thieu123@gmail.com>
2025-02-06 19:15:41 -0700

    get number of words and number of characters from txt file

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello world")
+def get_num_words(text):
+    words = text.split()
+    print(len(words))
+
+def get_chars_dict(text):
+    lower_text = text.lower()
+    characters = {}
+    
+    for char in lower_text:
+        if char in characters:
+            characters[char] = characters[char] + 1
+        else:
+            characters[char] = 1
+    
+    print(characters)
+
+def main():
+    with open("./books/frankenstein.txt") as f:
+        file_contents = f.read()
+        number_of_words(file_contents)
+        number_of_characters(file_contents)
+main()
