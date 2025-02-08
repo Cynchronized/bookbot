b455ef69970b72ccad907de7210b17458f0bc670
Cynchronized <austin.thieu123@gmail.com>
2025-02-08 01:42:05 -0700

    Added comments

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+def main():
+    file_location = "./books/frankenstein.txt"
+    with open(file_location) as f:
+        file_contents = f.read()
+        num_words = get_num_words(file_contents)
+        chars_dict = get_chars_dict(file_contents)
+        list_of_dicts = dict_to_list(chars_dict)
+        get_report(num_words, list_of_dicts, file_location)
+
+# Takes in the text of a file
+#
+# Returns: number of words
 def get_num_words(text):
     words = text.split()
     return len(words)
 
+
+# Takes in the text of a file then creates a dictionary counting the number of each character
+#
+# Returns: Dictionary of characters and their frequency
 def get_chars_dict(text):
     lower_text = text.lower()
     characters = {}
@@ -12,8 +28,12 @@ def get_chars_dict(text):
         else:
             characters[char] = 1
 
-    return characters    
+    return characters
 
+# Converts a dictionary of characters into a list of dictionaries
+# Filters out non alphabetical characters and sorts numerically from largest to smallest
+#
+# Returns: a list of dictionaries
 def dict_to_list(dict):
     list = []
 
@@ -24,10 +44,12 @@ def dict_to_list(dict):
 
     return list
 
+# Sort by number
 def sort_on(dict):
     return dict["num"]
 
 
+# This generates a report of the frequency of alphabetical letters
 def get_report(num_words, list_dicts, file_location):
     print(f"--- Begin report of {file_location} ---")
     print(f"{num_words} words found in the document \n")  
@@ -38,14 +60,5 @@ def get_report(num_words, list_dicts, file_location):
     
 
     print("--- End report ---")
-
-def main():
-    file_location = "./books/frankenstein.txt"
-    with open(file_location) as f:
-        file_contents = f.read()
-        num_words = get_num_words(file_contents)
-        chars_dict = get_chars_dict(file_contents)
-        list_of_dicts = dict_to_list(chars_dict)
-        get_report(num_words, list_of_dicts, file_location)
 
 main()
